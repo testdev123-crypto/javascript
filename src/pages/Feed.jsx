ba2b17781cae9e3abcaa7d49f65d7e14496dbca6
testdev123-crypto <rohanpai4949@gmail.com>
2024-09-19 12:32:53 +0530

    Update Feed.jsx

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -5,6 +5,7 @@ import { getCategoryVideos } from "../redux/categorySlice";
 import { useDispatch, useSelector } from "react-redux";
 import timeSince from "../utils/date";
 import "./feed.css";
+
 function Feed() {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -19,12 +20,14 @@ function Feed() {
       )
     );
     document.title = `${id ? id + "- Youtube" : "Home - Youtube"}`;
-  }, [id]);
+  }, [dispatch, id]); // Include 'dispatch' in the dependency array
 
   useEffect(() => {
     document.body.style.backgroundColor = darkMode ? "#131417" : "#fff";
   }, [darkMode]);
+
   var aDay = 24 * 60 * 60 * 1000;
+  
   return (
     <>
       {/* <Sidebar /> */}
@@ -38,23 +41,21 @@ function Feed() {
           sidebarExtend ? "sm:pl-[180px]" : "sm:pl-[70px]"
         } feedGrid grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-[4%] pt-20 mx-3 sm:ml-4 md:pr-[28px] lg:pr-14 gap-y-6 max-w-[100%] bg-contain `}
       >
-        {categoryVideos?.map((e, index) => {
-          return (
-            <div key={index} style={{ marginTop: index === 0 ? "0px" : "0px" }}>
-              <VideoCard
-                key={index}
-                title={e.snippet.title}
-                thumbnail={e.snippet?.thumbnails?.medium?.url}
-                on={timeSince(
-                  new Date(Date.parse(e.snippet.publishedAt) - aDay)
-                )}
-                channel={e.snippet.channelTitle}
-                channelId={e.snippet.channelId}
-                videoId={e.id.videoId}
-              />
-            </div>
-          );
-        })}
+        {categoryVideos?.map((e, index) => (
+          <div key={index} style={{ marginTop: index === 0 ? "0px" : "0px" }}>
+            <VideoCard
+              key={index}
+              title={e.snippet.title}
+              thumbnail={e.snippet?.thumbnails?.medium?.url}
+              on={timeSince(
+                new Date(Date.parse(e.snippet.publishedAt) - aDay)
+              )}
+              channel={e.snippet.channelTitle}
+              channelId={e.snippet.channelId}
+              videoId={e.id.videoId}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
